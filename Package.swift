2e4323255c8bb64154ab60b8f9221afbd2783c87
Fraser Scott-Morrison <fraserscottmorrison@me.com>
2020-07-16 14:22:37 +1200

    Create Package.swift

---
+++ Package.swift
@@ -0,0 +1,15 @@
+import PackageDescription
+
+let package = Package(
+    name: "WKCookieWebView"
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "WKCookieWebView", targets: ["WKCookieWebView"])
+    ],
+    targets: [
+        .target(name: "WKCookieWebView", dependencies: [])
+    ],
+    swiftLanguageVersions: [.v5]
+)
