96f9aa55b79826a07533520a13e0010c7da86779
Fraser Scott-Morrison <fraserscottmorrison@me.com>
2020-07-16 14:58:31 +1200

    Missing comma

--- Package.swift
+++ Package.swift
@@ -2,7 +2,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "WKCookieWebView"
+    name: "WKCookieWebView",
     platforms: [
         .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
     ],
