33cf12f3778a48cad50efda6d88dab98050fa733
Fraser <fraserscottmorrison@me.com>
2020-07-20 22:05:01 +1200

    Add support for Swift Package Manager (#19)
    * Create Package.swift
    * Added spm target path
    * Added swift tools version
    * Trying swift-tools-version:5.0
    * Missing comma

---
+++ Package.swift
@@ -0,0 +1,16 @@
+// swift-tools-version:5.0
+import PackageDescription
+
+let package = Package(
+    name: "WKCookieWebView",
+    platforms: [
+        .macOS(.v10_10), .iOS(.v8), .tvOS(.v9), .watchOS(.v3)
+    ],
+    products: [
+        .library(name: "WKCookieWebView", targets: ["WKCookieWebView"])
+    ],
+    targets: [
+        .target(name: "WKCookieWebView", dependencies: [], path: "WKCookieWebView")
+    ],
+    swiftLanguageVersions: [.v5]
+)
